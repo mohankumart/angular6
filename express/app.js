5b64a21c35f0273de14a16db946bfcc027198424
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-04-03 15:07:24 +0530

    testing

--- express/app.js
+++ express/app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const express = require('express')
+
+var app = express()
+
+app.get('/', (req, res)=>{
+    res.send('Hello Mohan!!!!')
+})
+
+app.listen(3001, ()=>{
+    console.log('server started on 3001')
+})
